e483c7b45db7e69401f5d6df16d9a0602879e77b
cslcalderonswift <55804061+cslcalderonswift@users.noreply.github.com>
2019-12-16 19:09:16 -0600

    center falling (#18)

--- MathicalWaters/GameScene.swift
+++ MathicalWaters/GameScene.swift
@@ -14,6 +14,29 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     var won = true
+
+    var equations:[String: Int] =
+    ["7 + 3": 10,
+     "9 + 2": 11,
+    "6 + 18": 24,
+    "15 + 15": 30,
+    "3 + 2": 6,
+    "6 + 8": 14,
+    "32 + 35": 67,
+    "6 + 7": 13,
+    "60 + 30": 90,
+    "22 + 21": 43,
+    "8 + 4": 12,
+    "22 + 32": 54,
+    "5 + 100": 105,
+    "8 + 7": 15,
+    "44 + 45": 89,
+    "3 + 14": 17,
+    "29 + 43": 72,
+    "7 + 7": 14,
+    "7 + 14": 21,
+    "10 + 18": 38]
+    
     // 1
            let boat = SKSpriteNode(imageNamed: "Boat")
              
@@ -24,14 +47,17 @@ class GameScene: SKScene {
              boat.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
              // 4
              addChild(boat)
-            
-            
-            
+            run(SKAction.repeatForever(
+              SKAction.sequence([
+                SKAction.run(addRain),
+                SKAction.wait(forDuration: 1.0)
+                ])
+            ))
+
             if !won {
                 let gameOverScene = FinishScreen(size: self.size, won: false)
                 self.view?.presentScene(gameOverScene)
             }
-            
            }
     /*override func didMove(to view: SKView) {
         
@@ -62,4 +88,34 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    func random() -> CGFloat {
+      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return random() * (max - min) + min
+    }
+
+    func addRain() {
+      
+      // Create sprite
+        for (eq, _) in equations {
+            let rain = SKLabelNode(fontNamed: "Damascus")
+            rain.text = eq
+            rain.fontColor = .black
+            rain.fontSize = 11
+            let y = random(min: size.height + 1, max: size.height + rain.fontSize)
+            rain.position = CGPoint(x: size.width/2, y: y)
+            addChild(rain)
+            let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+            let actionMove = SKAction.move(to: CGPoint(x: size.width/2, y: boat.position.y + boat.size.height),
+                                           duration: TimeInterval(actualDuration))
+            let actionMoveDone = SKAction.removeFromParent()
+            rain.run(SKAction.sequence([actionMove, actionMoveDone]))
+        }
+      //let rain = SKSpriteNode(imageNamed: "monster")
+      
+      
+    }
+
 }
